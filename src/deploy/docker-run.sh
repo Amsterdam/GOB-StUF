3e075d02823d42f8dae238810dc05696949d5e0f
Frank Zonneveld <f.zonneveld@amsterdam.nl>
2021-09-22 10:59:45 +0200

    Wait for keycloack to be reachable before starting oath2-proxy.

--- src/deploy/docker-run.sh
+++ src/deploy/docker-run.sh
@@ -3,6 +3,8 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 set -x
 
+DOMAIN_NAME=$(echo "${OAUTH2_PROXY_OIDC_ISSUER_URL}" | sed -e 's|^[^/]*//||' -e 's|/.*$||')
+timeout 60 bash -c "until cat < /dev/null > /dev/tcp/${DOMAIN_NAME}/443; do sleep 5; done"
 # Secure endpoints
 ./oauth2-proxy --config oauth2-proxy.cfg 2>&1 | tee /var/log/oauth2-proxy/oauth2proxy.log &
 
