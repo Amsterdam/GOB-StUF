98c92c454c192bf6e0394aad7ece04eb0cf4ddfd
Roel Kramer <contact@roelkramer.nl>
2021-12-22 14:25:47 +0100

    Show inonderzoek only if value is J

--- src/tests/rest/brp/test_views.py
+++ src/tests/rest/brp/test_views.py
@@ -10,15 +10,22 @@ class TestIngeschrevenpersonenBsnView:
         assert list(response.json["verblijfplaats"])[1] == "adresseerbaarObjectIdentificatie"
         assert response.json["verblijfplaats"]["adresseerbaarObjectIdentificatie"] == "0518010000784987"
 
-    @pytest.mark.parametrize("stuf_310_response", ["response_310_in_onderzoek.xml"], indirect=True)
-    def test_in_onderzoek_true(self, stuf_310_response, app_base_path, client, jwt_header, monkeypatch):
-        """Make sure inOnderzoek is set when it is in XML."""
+    @pytest.mark.parametrize("stuf_310_response", ["response_310_in_onderzoek_j.xml"], indirect=True)
+    def test_in_onderzoek_j(self, stuf_310_response, app_base_path, client, jwt_header, monkeypatch):
+        """Make sure inOnderzoek is set when it ha value J in XML."""
         response = client.get(f"{app_base_path}/brp/ingeschrevenpersonen/123456789", headers=jwt_header)
         assert response.status_code == 200
         assert response.json["verblijfplaats"]["inOnderzoek"]
 
+    @pytest.mark.parametrize("stuf_310_response", ["response_310_in_onderzoek_n.xml"], indirect=True)
+    def test_in_onderzoek_n(self, stuf_310_response, app_base_path, client, jwt_header, monkeypatch):
+        """Make sure inOnderzoek is not set when it has value N in XML."""
+        response = client.get(f"{app_base_path}/brp/ingeschrevenpersonen/123456789", headers=jwt_header)
+        assert response.status_code == 200
+        assert "inOnderzoek" not in response.json["verblijfplaats"]
+
     @pytest.mark.parametrize("stuf_310_response", ["response_310.xml"], indirect=True)
-    def test_in_onderzoek_false(self, stuf_310_response, app_base_path, client, jwt_header, monkeypatch):
+    def test_in_onderzoek_missing(self, stuf_310_response, app_base_path, client, jwt_header, monkeypatch):
         """Make sure inOnderzoek is not set when it is not in XML."""
         response = client.get(f"{app_base_path}/brp/ingeschrevenpersonen/123456789", headers=jwt_header)
         assert response.status_code == 200
