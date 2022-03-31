6106e1b803084149f0c5df2211565e1c8abb5ce3
Roel Kramer <contact@roelkramer.nl>
2022-03-31 15:32:38 +0200

    Add docker-compose based workflow (#280)
    * Add docker-compose based workflow
    
    * Fix Jenkinsfile

--- src/test.sh
+++ src/test.sh
@@ -3,8 +3,7 @@
 set -u # crash on missing env
 set -e # stop on any error
 
-# Clear any cached results
-find . -name "*.pyc" -exec rm -f {} \;
+export COVERAGE_FILE=/tmp/.coverage
 
 echo "Running tests"
 coverage run --source=./gobstuf -m pytest tests/
